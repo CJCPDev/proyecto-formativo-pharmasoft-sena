8f38d546d98ab235a6f3f331598954029030a40f
CJCPDev <carmonacamilo53@gmail.com>
2026-02-12 18:38:49 -0500

    feat: estructura base del proyecto

---
+++ frontend/sena-pharmasoft/src/app/App.jsx
@@ -0,0 +1,9 @@
+export default function App(){
+  return (
+    <div className="flex items-center text-6xl justify-center bg-green-900">
+      <h1 className="text-white">
+        ¡Hola mundo!
+      </h1>
+    </div>
+  )
+}
