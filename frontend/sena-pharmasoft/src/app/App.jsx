2ec5a018cf4798d19420c486147ad0b9e3a2cc00
luis <luisdavidguevam20041017@gmail.com>
2026-02-20 20:17:18 -0500

    feat: actualizacion rama

--- frontend/sena-pharmasoft/src/app/App.jsx
+++ frontend/sena-pharmasoft/src/app/App.jsx
@@ -3,8 +3,8 @@ import UserForm from '../features/users/components/UserForm'
 export default function App(){
   return (
     <div className="grid grid-cols-1 items-center gap-6 justify-center justify-items-center">
-      <h1 className="text-black">
-        ¡Hola mundo!
+      <h1 className="text-green-800 font-bold text-2xl">
+        Creación de usuario
       </h1>
       <div>
        <UserForm/>
--- frontend/sena-pharmasoft/src/features/users/components/UserForm.jsx
+++ frontend/sena-pharmasoft/src/features/users/components/UserForm.jsx
@@ -1,15 +1,36 @@
-import Input from './../../../shared/components/Input'
+import Input from "./../../../shared/components/Input";
 
-export default function UserForm(){
-    return (
-        <div>
-            {/* Formulario para crear el usuario*/}
-            <form>
-                <Input
-                label='Nombre'
-                placeholder='Ingrese su nombre'
-                />
-            </form>
-        </div>
-    )
+export default function UserForm() {
+  return (
+      <div>
+        <form className="grid grid-cols-2 gap-6 w-full max-w-4xl">
+          {/* Formulario para crear el usuario*/}
+          <Input
+            label="Número de documento"
+            placeholder="Número de documento"
+          />
+          <Input
+          label="Nombre completo"
+          placeholder="Nombre completo" />
+          <Input
+            label="Correo electronico"
+            type="email"
+            placeholder="Correo electronico"
+          />
+          <Input
+            label="Confirmar correo electronico"
+            type="email"
+            placeholder="Confirmar correo electronico"
+          />
+          <Input 
+          label="Dirección"
+          placeholder="Dirección"
+          />
+          <Input 
+          label="Celular"
+          type="tel"
+          placeholder="Celular" />
+        </form>
+      </div>
+  );
 }
