3e93350a709ac341170f7540c1a1d050230e9a8f
CJCPDev <carmonacamilo53@gmail.com>
2026-02-27 23:55:48 -0500

    feat: actualizacion de estilos y componentes

--- frontend/sena-pharmasoft/src/features/users/components/UserForm.jsx
+++ frontend/sena-pharmasoft/src/features/users/components/UserForm.jsx
@@ -1,3 +1,21 @@
 import Input from '@/shared/components/Input'
 import Button from '@/shared/components/Button'
+import Select from '@/shared/components/Select'
 
+export default function Prueba(){
+
+
+    return(
+        <div>
+            <Input
+                label='hola'
+
+            />
+
+            <Select
+            string='seleccione su documento'
+            
+            ></Select>
+        </div>
+    )
+}
--- frontend/sena-pharmasoft/src/shared/components/Button.jsx
+++ frontend/sena-pharmasoft/src/shared/components/Button.jsx
@@ -7,8 +7,8 @@ export default function Button ({
 }){
 
     const variants = {
-        primary: 'border border-border  bg-brand text-inverse hover:bg-brand-hover',
-        secondary: 'border border-border-strong bg-brand text-primary hover:bg-brand-soft',
+        primary: 'font-main text-brand-soft font-semibold text-base bg-brand-hover hover:bg-brand-soft hover:text-brand-hover',
+        secondary: "font-main text-brand-hover font-semibold text-base bg-brand-soft hover:bg-brand-hover hover:text-brand-soft",
     };
 
     const sizes = {
@@ -35,9 +35,10 @@ export default function Button ({
         <button
         type = {type}
         className={`
+            w-40
             relative
             inline-flex items-center justify-center
-            rounded-md
+            rounded-xl
             transition-colors
             ${variants[variant]}
             ${sizes[size]}
