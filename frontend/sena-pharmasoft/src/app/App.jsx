bb520ad75f1c35df8e294e7ae32feeb9f1e51f76
CJCPDev <carmonacamilo53@gmail.com>
2026-02-28 19:34:52 -0500

    feat: actualizacion de estados, router y usuarios

--- frontend/sena-pharmasoft/src/app/App.jsx
+++ frontend/sena-pharmasoft/src/app/App.jsx
@@ -1,14 +1,14 @@
-import UserForm from '../features/users/components/UserForm'
+import { RouterProvider } from 'react-router-dom';
+import router from '../app/router/router';
+
+
+
 
 export default function App(){
-  return (
-    <div className="grid grid-cols-1 items-center gap-6 justify-center justify-items-center">
-      <h1 className="text-black">
-        ¡Hola mundo!
-      </h1>
-      <div>
-       <UserForm/>
-      </div>
-    </div>
+  
+  
+  return (  
+    <RouterProvider router={router}/>
+
   )
 }
---
+++ frontend/sena-pharmasoft/src/features/users/components/ForgetPassword.jsx
@@ -0,0 +1,11 @@
+
+
+export default function ForgetPassword(){
+
+
+    return(
+        <div>
+            <h1>prueba olvido su contraseña</h1>
+        </div>
+    )
+}
