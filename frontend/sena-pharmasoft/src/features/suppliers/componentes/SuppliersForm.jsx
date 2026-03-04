d8c2f8d759cbae4c68bf3a59e3fc871a16a706b0
CJCPDev <carmonacamilo53@gmail.com>
2026-03-03 22:40:47 -0500

    feat: actualizacion

--- frontend/sena-pharmasoft/src/features/suppliers/componentes/SuppliersForm.jsx
+++ frontend/sena-pharmasoft/src/features/suppliers/componentes/SuppliersForm.jsx
@@ -20,85 +20,74 @@ export default function SuppliersForm (){
 
     return (
         // {/* Formulario para crear proveedores */}
-        <form className="flex flex-col gap-6 w-175 px-4 py-6 ">
-                {/* Contenedor de inputs y selects */}
-                <div className="flex flex-col gap-3 w-full">  
-                    <div className="flex gap-4">   {/*fila 1 (contenedor de 2 input)*/}
-                        <Input
-                            label = "NIT"
-                            placeholder = "Ingrese el NIT de la empresa"
-                            >
-                        </Input>
-                    
-                        <Input
-                            label = "Documento de identidad"
-                            placeholder = "Ingrese el documento"
-                            >
-                        </Input>
-                    </div>
-                    <div className="flex gap-4"> {/*fila 2 (contenedor de 1 input)*/}
-                        <Input
-                            label = "Razón social"
-                            placeholder = "Ingrese la razón social"
-                            >
-                        </Input>
-                        <Input
-                            label = "Dirección"
-                            placeholder = "Ingrese la dirección del proveedor"
-                            >
-                        </Input>
-                    </div>
-                    <div className="flex "> {/*fila 3 (contenedor de 2 input y 1 select)*/}
-                        <Input
-                            label = "Teléfono de contacto"
-                            placeholder = "Ingrese el número de teléfono"
-                            type = "tel"
-                            >
-                        </Input>
-                     
-                        <Input
-                            label = "Ciudad"
-                            placeholder = "Ingrese la ciudad"
-                            >
-                        </Input>
-        
-                        <Select
-                            label= "Estado"
-                            name = "suppliersState"
-                            options = {suppliersState}
-                        >
-                        </Select>
-                    </div>
-                    <div className="flex gap-4">   {/*fila 5 (contenedor de 1 input)*/}
-                        <Input
-                            label = "Correo electrónico"
-                            placeholder = "Ingrese el correo del contacto"
-                            type = "email"
-                            >
-                        </Input>
-                        <Input
-                            label = "Nombre del contacto"
-                            placeholder = "Ingrese el nombre del contacto"
-                            >
-                        </Input>
-                    </div>
-                </div> 
-                <div className="flex gap-6 justify-center items-center"> {/*contenedor de los 2 botones*/}
-                    <Button
-                        variant = "secondary"
-                        size = "sm"
+        <form className="flex flex-col gap-6 w-175 px-4 py-6">
+            <h1 className="w-full text-center text-text-primary font-bold text-3xl">
+                Crear Proveedor
+            </h1>
 
-                    >
-                        Regresar
-                    </Button>
-                    <Button
-                        variant = "primary"
-                        size = "md"
-                        type = "submit"
-                    >
-                        Crear
-                    </Button>
+            <div className="grid grid-cols-2 gap-x-4 gap-y-1.5">
+                
+                <Input
+                label="NIT"
+                placeholder="Ingrese el NIT de la empresa"
+                />
+
+                <Input
+                label="Documento de identidad"
+                placeholder="Ingrese el documento"
+                />
+
+                <Input
+                label="Razón social"
+                placeholder="Ingrese la razón social"
+                />
+
+                <Input
+                label="Dirección"
+                placeholder="Ingrese la dirección del proveedor"
+                />
+
+                <div className="col-span-2">
+                    <Input
+                    label="Correo electrónico"
+                    placeholder="Ingrese el correo del contacto"
+                    type="email"
+                    />
                 </div>
-            </form>
+
+                <Input
+                label="Teléfono de contacto"
+                placeholder="Ingrese el número de teléfono"
+                type="tel"
+                />
+
+                <Select
+                label="Estado"
+                name="suppliersState"
+                options={suppliersState}
+                />
+
+                <Input
+                label="Ciudad"
+                placeholder="Ingrese la ciudad"
+                />
+
+                <Input
+                label="Nombre del contacto"
+                placeholder="Ingrese el nombre del contacto"
+                />
+
+            </div>
+
+            <div className="flex gap-6 justify-center items-center">
+                <Button variant="secondary" size="sm">
+                Regresar
+                </Button>
+                <Button variant="primary" size="md" type="submit">
+                Crear
+                </Button>
+            </div>
+
+        </form>
     )
 }
