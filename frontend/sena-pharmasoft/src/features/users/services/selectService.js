54f9f996b61e6a2245da8003219edcebbcaa0e64
CJCPDev <carmonacamilo53@gmail.com>
2026-03-07 15:57:52 -0500

    feat: Se agrega componente Card, index.js en los modulos para sus respectivas APIS publicas

--- frontend/sena-pharmasoft/src/features/users/services/selectService.js
+++ frontend/sena-pharmasoft/src/features/users/services/selectService.js
@@ -1,6 +1,6 @@
 import documentTypes from "../../../data/selects/documentTypes.json";
 import userGroups from "../../../data/selects/userGroups.json"
-import pharmaForm from '../../../data/selects/pharmaForm.json'
+/* import pharmaForm from '../../../data/selects/pharmaForm.json' */
 
 export async function getDocumentTypes(){     
     return documentTypes();
