506aba478c06ba07542fed949b5ae85b7401c0a0
CJCPDev <carmonacamilo53@gmail.com>
2026-03-13 20:09:34 -0500

    feat: actualizacion

--- frontend/sena-pharmasoft/src/shared/components/AvatarUploader.jsx
+++ frontend/sena-pharmasoft/src/shared/components/AvatarUploader.jsx
@@ -1,12 +1,12 @@
 import FileInput from "./FileInput";
 
 // 
-export default function AvatarUploader({ onChange }) {
+export default function AvatarUploader({ onChange,label }) {
     
 return (
         <FileInput
         // Etiqueta mostrada al usuario
-        label="Foto de perfil"
+        label={label}
         // Tipos de archivo permitidos (PNG y JPEG)
         accept="image/png, image/jpeg"
         // Callback que propaga el archivo seleccionado al componente padre
