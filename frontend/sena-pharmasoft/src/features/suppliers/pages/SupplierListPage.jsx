2b2be5815b45c631e5784744f5bd4f341785fa04
Fede01_code <alarconpachonfederico@gmail.com>
2026-03-19 22:46:00 -0500

    feat: creación del los reportes

--- frontend/sena-pharmasoft/src/features/suppliers/pages/SupplierListPage.jsx
+++ frontend/sena-pharmasoft/src/features/suppliers/pages/SupplierListPage.jsx
@@ -2,12 +2,15 @@ import { Button, Title, DataTable } from "@/shared/components"
 import { Link, useNavigate } from "react-router-dom"
 import { SupplierColumns } from "../table/SupplierColumns"
 import { suppliers } from "@/data/suppliers/suppliers"
+import { useState } from "react"
+import  ReportConfigModal  from "../reports/components/ReportConfigModal"
 
 export default function SuppliersListPage (){
     const navigate = useNavigate()
+    const [isReportModalOpen, setIsReportModalOpen] = useState(false)
     return(
         <div
-            className="relative grid gap-6 bg-white rounded-xl shadow-2xl p-4
+            className="relative grid gap-6 bg-white rounded-xl shadow-2xl p-4 w-350
         ">
             <Title
                 title="Proveedores"
@@ -18,18 +21,21 @@ export default function SuppliersListPage (){
                         Crear Proveedor
                     </Link>
                 </div>
-                <div className="border rounded-xl h-12 w-auto px-4 flex items-center">
-                    <Link to="/reportar-proveedor" className="hover:text-text-primary transition hover:underline hover:underline-offset-2">
-                        Generar Reporte
-                    </Link>
+                <div>
+                   <Button
+                        variant= "primary"
+                        onClick= {() => setIsReportModalOpen(true)}
+                    >
+                        Generar reporte
+                    </Button>
+
+                    <ReportConfigModal
+                        isOpen= {isReportModalOpen}
+                        onClose= {() => setIsReportModalOpen(false)}
+                    />
                 </div>
             </div>
             <div className="flex gap-6">
-                {/* <div className="w-100 h-full border-2 border-black rounded-3xl">
-                    <table> 
-
-                    </table>
-                </div> */}
                 <div className="w-full h-full  p-4">
                     <DataTable
                         data={suppliers}
---
+++ frontend/sena-pharmasoft/src/features/suppliers/reports/config/supplierReportFields.js
@@ -0,0 +1,42 @@
+export const supplierReportFields = [
+    {
+        key: "id",
+        label: "ID",
+        default: true
+    },
+    {
+        key: "nit",
+        label: "NIT",
+        default: true
+    },
+    {
+        key: "razonSocial",
+        label: "Razón Social",
+        default: true
+    },
+    {
+        key: "direccion",
+        label: "Dirección",
+        default: false
+    },
+    {
+        key: "correo",
+        label: "Correo Electronico",
+        default: true
+    },
+    {
+        key: "telContacto",
+        label: "Teléfono del Contacto",
+        default: true
+    },
+    {
+        key: "ciudad",
+        label: "Ciudad",
+        default: true
+    },
+    {
+        key: "estado",
+        label: "Estado",
+        default: true
+    }
+]
