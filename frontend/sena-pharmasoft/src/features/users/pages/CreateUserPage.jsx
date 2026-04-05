5354e872cf9b874928b151625328f96ea5015632
CJCPDev <carmonacamilo53@gmail.com>
2026-04-05 18:45:20 -0500

    update: actualizacion

--- frontend/sena-pharmasoft/src/features/users/pages/CreateUserPage.jsx
+++ frontend/sena-pharmasoft/src/features/users/pages/CreateUserPage.jsx
@@ -4,7 +4,6 @@ export default function CreateUserPage (){
     return(
         <div
             className="relative bg-white  rounded-xl shadow-2xl p-4">
-                <h1>🔥 USER FORM RENDER</h1>
             <UserForm/>
         </div>
         
