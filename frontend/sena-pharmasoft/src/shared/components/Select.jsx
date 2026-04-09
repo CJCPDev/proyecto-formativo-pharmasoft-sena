d11e6ff51bec137ad39cab7371ba395a114410c7
CJCPDev <carmonacamilo53@gmail.com>
2026-04-09 18:44:43 -0500

    feat: actualizacion

--- frontend/sena-pharmasoft/src/shared/components/Select.jsx
+++ frontend/sena-pharmasoft/src/shared/components/Select.jsx
@@ -5,6 +5,7 @@ export default function Select({
   value,
   error,
   onChange,
+  disabled,
 }) {
   return (
     <div className="w-full flex flex-col">
@@ -25,23 +26,20 @@ export default function Select({
         name={name}
         value={value}
         onChange={onChange}
+        disabled={disabled}
         className={`
           w-full h-10 px-4
           rounded-xl
           border
           text-base
           transition
-
           bg-brand-soft/60
-
           ${error ? "border-red-600" : "border-border-strong"}
-
           focus:outline-none
           focus:ring-1
           focus:ring-brand-hover
           focus:border-brand-hover
           focus:bg-white
-
           hover:bg-white
           hover:border-brand-hover
         `}
@@ -57,7 +55,6 @@ export default function Select({
         ))}
       </select>
 
-      {/* 🔥 ESPACIO FIJO PARA ERROR */}
       <p className="text-red-700 text-xs mt-1 h-4">{error || ""}</p>
     </div>
   );
