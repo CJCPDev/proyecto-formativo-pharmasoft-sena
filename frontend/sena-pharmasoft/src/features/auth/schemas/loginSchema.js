04bce76360f5f1550cf412a0f56d469e5c242aaa
Fede01_code <alarconpachonfederico@gmail.com>
2026-04-08 20:07:51 -0500

    feat: actualizacion de validaciones

--- frontend/sena-pharmasoft/src/features/auth/schemas/loginSchema.js
+++ frontend/sena-pharmasoft/src/features/auth/schemas/loginSchema.js
@@ -7,9 +7,9 @@ export const loginSchema = z.object({
 
     password: z
         .string()
-        .min(8, "Debe tener al menos 8 caracteres")
-        .regex(/[A-Z]/, "Debe contener al menos una mayúscula")
-        .regex(/[0-9]/, "Debe contener al menos un número")
+        .min(8, "Debe tener al menos 6 caracteres")
+        // .regex(/[A-Z]/, "Debe contener al menos una mayúscula")
+        // .regex(/[0-9]/, "Debe contener al menos un número")
 
  
 })
