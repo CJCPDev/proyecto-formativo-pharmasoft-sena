60bfe37a5823e5d02f80282a4ec086d43a2bfb64
luis <luisdavidguevam20041017@gmail.com>
2026-04-10 12:57:55 -0500

    feat: actualizacon de modulos ya listos

---
+++ pharmasoft_backend/suppliers/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from suppliers.models import Suppliers, Departamento, Municipio
+
+
+class SupplierSerializer(serializers.ModelSerializer):
+    razonSocial = serializers.CharField(source='razon_social')
+    telefonoContacto = serializers.CharField(source='telefono_contacto')
+    nombreContacto = serializers.CharField(source='nombre_contacto')
+    nombre = serializers.CharField(source='nombre_proveedor')
+    correo = serializers.CharField(source='correo_contacto')
+    ciudad = serializers.PrimaryKeyRelatedField(
+        source='id_municipio',
+        queryset=Municipio.objects.all()
+    )
+    departamento = serializers.PrimaryKeyRelatedField(  # 👈 agregado
+        source='id_departamento',
+        queryset=Departamento.objects.all()
+    )
+    nombreCiudad = serializers.CharField(source='id_municipio.nombre_municipio', read_only=True)
+    nombreDepartamento = serializers.CharField(source='id_departamento.nombre_departamento', read_only=True)
+
+    class Meta:
+        model = Suppliers
+        fields = [
+            'id',
+            'nit',
+            'nombre',
+            'razonSocial',
+            'direccion',
+            'correo',
+            'telefonoContacto',
+            'estado',
+            'ciudad',
+            'departamento',
+            'nombreCiudad',
+            'nombreDepartamento',
+            'nombreContacto'
+        ]
+
+class DepartamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departamento
+        fields = "__all__" 
+class MunicipioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Municipio
+        fields = "__all__" 
