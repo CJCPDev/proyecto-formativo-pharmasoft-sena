637880d8d79495af9bd794b9e56a7f89aaa6b0b0
CJCPDev <carmonacamilo53@gmail.com>
2026-04-10 23:44:09 -0500

    feat: responsive 10.04-11.43

--- frontend/sena-pharmasoft/src/features/sales/pages/ListSalePage.jsx
+++ frontend/sena-pharmasoft/src/features/sales/pages/ListSalePage.jsx
@@ -7,14 +7,16 @@ import ReportConfigModal from "../reports/components/ReportConfigModal";
 export default function ListSalePage() {
   const navigate = useNavigate();
   const [IsReportModalOpen, setIsReportModalOpen] = useState(false);
-  const [selectedSale, setSelectedSale] = useState(null);
-const [mode, setMode] = useState(null); // "view" | "edit" | null
 
   return (
-    <div className="relative grid gap-6 bg-white rounded-xl shadow-2xl p-4 w-350 h-150">
+    <div className="relative grid gap-6 bg-white rounded-xl shadow-2xl p-4 w-full max-w-7xl mx-auto">
       <Title title="Modulo de Ventas" />
-      <div className="flex justify-between gap-6 items-center">
-        <div className="flex px-4">
+
+      {/* HEADER */}
+      <div className="flex flex-col md:flex-row justify-between gap-4 items-start md:items-center">
+        
+        {/* IZQUIERDA */}
+        <div className="flex flex-wrap gap-2">
           <Button
             variant="secondary"
             size="sm"
@@ -26,22 +28,30 @@ const [mode, setMode] = useState(null); // "view" | "edit" | null
           <ReportConfigModal
             isOpen={IsReportModalOpen}
             onClose={() => setIsReportModalOpen(false)}
-          ></ReportConfigModal>
+          />
         </div>
-        <div className="flex px-10 gap-6 items-center">
-          <Button variant="primary" onClick={() => setIsReportModalOpen(true)}>
+
+        {/* DERECHA */}
+        <div className="flex flex-col sm:flex-row gap-3 w-full md:w-auto">
+          <Button
+            variant="primary"
+            onClick={() => setIsReportModalOpen(true)}
+          >
             Generar reporte
           </Button>
+
           <Link
-            className="w-40 relative inline-flex items-center justify-center rounded-xl transition-colors cursor-pointer
-                        h-10 px-4 before:absolute before:content-[''] before:-inset-y-[4px] before:-inset-x-[0px] font-main text-brand-soft font-semibold text-base bg-brand-hover hover:bg-brand-soft hover:text-brand-hover"
             to="/crear-venta"
+            className="w-full sm:w-40 flex items-center justify-center rounded-xl h-10 px-4 font-semibold
+            bg-brand-hover text-brand-soft hover:bg-brand-soft hover:text-brand-hover transition-colors"
           >
             Crear Venta
           </Link>
         </div>
       </div>
-      <div className="flex gap-6">
+
+      {/* CONTENIDO */}
+      <div className="overflow-x-auto">
         <SaleReportPage />
       </div>
     </div>
