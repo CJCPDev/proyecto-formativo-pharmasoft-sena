ea17c5d4c585ccd76bf78bbb77ab76e359d429b6
Mateossa <mateo.ossa101@gmail.com>
2026-04-10 22:49:38 -0500

    feat: actualizacion

--- frontend/sena-pharmasoft/src/features/products/admin/pages/CreateProductPage.jsx
+++ frontend/sena-pharmasoft/src/features/products/admin/pages/CreateProductPage.jsx
@@ -1,18 +1,4 @@
-// import React from "react";
-// import AdminProductForm from "../components/AdminProductForm";
-// import { useLocation } from "react-router-dom";
 
-// export default function CreateProductPage() {
-//   const location = useLocation();
-//   const onCreated = location.state?.onCreated;
-
-//   return (
-//     <div className="relative grid gap-6 bg-white rounded-xl shadow-2xl p-4">
-//       {/*  Pasamos la función al formulario */}
-//       <AdminProductForm onCreated={onCreated} />
-//     </div>
-//   );
-// }
 // React es necesario en archivos que contienen JSX
 import React from "react";
 
