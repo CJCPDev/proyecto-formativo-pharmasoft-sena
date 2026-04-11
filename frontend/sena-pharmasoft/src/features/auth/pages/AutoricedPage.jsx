e88f1c734cbbabeb88ee7fa27d256df45057133e
CJCPDev <carmonacamilo53@gmail.com>
2026-04-11 04:02:33 -0500

    feat: responsive 11.04 - 3.05

--- frontend/sena-pharmasoft/src/features/auth/pages/AutoricedPage.jsx
+++ frontend/sena-pharmasoft/src/features/auth/pages/AutoricedPage.jsx
@@ -1,7 +1,12 @@
 export default function AutoricedPage() {
   return (
-    <div className="mx-auto">
-      <h1>hola</h1>
+    <div className="min-h-screen bg-white flex flex-col items-end">
+
+
+      <div className="border-t p-4 text-center text-xs text-gray-400">
+        © 2026 Pharmasoft - Sistema de farmacia
+      </div>
+
     </div>
   );
 }
